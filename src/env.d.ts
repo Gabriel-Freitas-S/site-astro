93066ba4acde6f3a37f87220a529e2bc0a5fb471
GabrielFS <gabriel_freitas_souza_@outlook.com>
2025-02-14 16:03:31 -0300

    refatora estrutura e acessibilidade das páginas de certificações e experiência, adiciona ordenação e descrição dinâmica

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,33 +1,6 @@
 /// <reference types="astro/client" />
 
-declare module 'astro:content' {
-  interface Schemas {
-    certifications: {
-      type: 'data';
-      data: {
-        title: string;
-        duration: string;
-        courses: {
-          name: string;
-          period?: string;
-          duration: string;
-        }[];
-        complementaryContent?: {
-          name: string;
-          duration: string;
-        }[];
-      };
-    };
-    experience: {
-      type: 'data';
-      data: {
-        company: string;
-        position: string;
-        period: string;
-        description: string;
-        technologies?: string[];
-        achievements?: string[];
-      };
-    };
-  }
+interface Window {
+    openPopup: (id: string) => void;
+    closePopup: (id: string) => void;
 }
