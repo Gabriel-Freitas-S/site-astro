3bcd73f8399f173d1903f913372106b2e08ab44d
Gabriel-Freitas-S <151963078+Gabriel-Freitas-S@users.noreply.github.com>
2025-02-15 00:49:21 -0300

    adiciona novas dependências para sitemap e analytics, refatora configurações de integração para otimização

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,25 @@ import { defineConfig } from 'astro/config';
 import compressor from 'astro-compressor';
 import minify from 'astro-minify-html-swc';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    compressor({
-      gzip: true,
-      brotli: true,
-      fileExtensions: [
-        '.css',
-        '.js',
-        '.html',
-        '.xml',
-        '.cjs',
-        '.mjs',
-        '.svg',
-        '.txt',
-        '.json',
-        '.ts'
+  integrations: [compressor({
+    gzip: true,
+    brotli: true,
+    fileExtensions: [
+      '.css',
+      '.js',
+      '.html',
+      '.xml',
+      '.cjs',
+      '.mjs',
+      '.svg',
+      '.txt',
+      '.json',
+      '.ts'
 
-      ]
-    }),
-    minify()
-  ]
+    ]
+  }), minify(), sitemap()]
 });
