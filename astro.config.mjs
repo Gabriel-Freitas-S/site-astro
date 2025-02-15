9cd2a7d7e4f7668974ad546f172853b93ff2e499
Gabriel-Freitas-S <151963078+Gabriel-Freitas-S@users.noreply.github.com>
2025-02-14 22:32:36 -0300

    adiciona novas dependências para otimização e minificação, refatora o cabeçalho para melhor responsividade e remove componente de boas-vindas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import compressor from 'astro-compressor';
+import minify from 'astro-minify-html-swc';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    compressor({
+      gzip: true,
+      brotli: true,
+      fileExtensions: [
+        '.css',
+        '.js',
+        '.html',
+        '.xml',
+        '.cjs',
+        '.mjs',
+        '.svg',
+        '.txt',
+        '.json',
+        '.ts'
+
+      ]
+    }),
+    minify()
+  ]
+});
