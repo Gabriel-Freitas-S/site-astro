449e36ffbabe8026908adb4f31984e191d668b87
GabrielFS <gabriel_freitas_souza_@outlook.com>
2025-02-18 10:52:39 -0300

    adiciona novas habilidades e tecnologias nas certificações, atualiza a estrutura de dados e implementa filtro de habilidades na página de certificações

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,35 +1,49 @@
 import { defineCollection, z } from 'astro:content';
 
-const certificationsCollection = defineCollection({
-  type: 'data',
-  schema: z.object({
-    title: z.string(),
-    duration: z.string(),
-    courses: z.array(z.object({
-      name: z.string(),
-      period: z.string().optional(),
-      duration: z.string()
-    })),
-    complementaryContent: z.array(z.object({
-      name: z.string(),
-      duration: z.string()
-    })).optional()
-  })
+const certificationCollection = defineCollection({
+    type: 'data',
+    schema: z.object({
+        title: z.string(),
+        duration: z.string(),
+        skills: z.array(
+            z.object({
+                title: z.string(),
+                technologies: z.array(
+                    z.object({
+                        name: z.string(),
+                        icon: z.string()
+                    })
+                )
+            })
+        ),
+        courses: z.array(
+            z.object({
+                name: z.string(),
+                period: z.string().optional(),
+                duration: z.string()
+            })
+        ),
+        complementaryContent: z.array(
+            z.object({
+                name: z.string(),
+                duration: z.string()
+            })
+        ).optional()
+    })
 });
 
 const experienceCollection = defineCollection({
-  type: 'data',
-  schema: z.object({
-    company: z.string(),
-    position: z.string(),
-    period: z.string(),
-    description: z.string(),
-    technologies: z.array(z.string()).optional(),
-    achievements: z.array(z.string()).optional()
-  })
+    type: 'data',
+    schema: z.object({
+        company: z.string(),
+        position: z.string(),
+        period: z.string(),
+        description: z.string(),
+        achievements: z.array(z.string()).optional()
+    })
 });
 
 export const collections = {
-  certifications: certificationsCollection,
-  experience: experienceCollection
+    'certifications': certificationCollection,
+    'experience': experienceCollection
 };
